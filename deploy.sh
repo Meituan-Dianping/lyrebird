dc29d9ff94c238d441cdf2b05f1d28a756250c33
zhaoye <zy81218@gmail.com>
2018-07-16 12:37:05 +0800

    fix gh-pages deploy script

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhaoye/lyrebird.git master:gh-pages
+git push -f git@github.com:meituan/lyrebird.git master:gh-pages
 
 cd -
