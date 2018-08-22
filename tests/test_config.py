00f2b29e8008bcf589a31ffe75e5bb7682f34244
zhaoye <zy81218@gmail.com>
2018-08-22 15:08:02 +0800

    fix class name

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,10 @@
-from lyrebird import nconfig
+from lyrebird import config
 from pathlib import Path
 
 
 def test_create(tmpdir):
-    cm = nconfig.ConfigManager(conf_root_path=tmpdir)
-    assert str(cm.conf_path) == str(tmpdir) + '/conf.json'
-    assert cm.conf_path.exists()
+    cm = config.ConfigManager(conf_root_path=tmpdir)
+    assert str(cm.conf_file) == str(tmpdir) + '/conf.json'
+    assert cm.conf_file.exists()
     assert cm.config
-    assert cm.config == nconfig.config_template
+    assert cm.config == config.config_template
