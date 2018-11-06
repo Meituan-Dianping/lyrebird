99cc0be194535a74c8c3986cd23b81f2f1a53802
zhaoye <zy81218@gmail.com>
2018-11-06 17:27:28 +0800

    import iview

--- lyrebird-fe/src/components/HelloWorld.vue
+++ lyrebird-fe/src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <Button type="primary">Primary</Button>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
--- lyrebird-fe/src/main.js
+++ lyrebird-fe/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import iView from 'iview'
 import App from './App.vue'
 import store from './store'
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+Vue.use(iView)
 
 new Vue({
   store,
