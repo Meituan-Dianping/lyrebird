6913a389301c531026d414084047dad8f5140ac0
zhaoye <zy81218@gmail.com>
2018-11-07 10:21:05 +0800

    add vue-router

--- lyrebird-fe/src/main.js
+++ lyrebird-fe/src/main.js
@@ -3,11 +3,13 @@ import iView from 'iview'
 import App from './App.vue'
 import store from './store'
 import 'iview/dist/styles/iview.css';
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(iView)
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
