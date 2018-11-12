1346f2cd15dbca46a7f7fad60e2ecc34bcb6ec5a
PearlSumRise <pearlsumrise@gmail.com>
2018-11-12 11:02:04 +0800

    front-end use iview layout

--- lyrebird-fe/src/router.js
+++ lyrebird-fe/src/router.js
@@ -18,6 +18,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/hello',
+      name: 'hello',
+      component: () => import(/* webpackChunkName: "about" */ './views/Hello.vue')
+    },
   ]
 })
