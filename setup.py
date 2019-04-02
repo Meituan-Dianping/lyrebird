d8a2e3d3dd9a6ba878264b9816cffebc4957fb03
zhaoye <zhaoye@users.noreply.github.com>
2019-04-02 19:38:05 +0800

    v1.6.2 - bugfix (#133)
    bugfix: crash after handle encoded URL
    upgrade werkzeug to 0.15.1
    lock all requirements version

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ VERSION = runpy.run_path(
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.readlines()
+
 setup(
     name='lyrebird',
     version=VERSION,
@@ -32,16 +35,5 @@ setup(
             'lyrebird = lyrebird.manager:main'
         ]
     },
-    install_requires=[
-        'mitmproxy==4.0.4',
-        'flask==1.0.2',
-        'requests==2.21.0',
-        'colorama==0.4.1',
-        'flask-socketio==3.3.1',
-        'flask-restful==0.3.7',
-        'beautifulsoup4==4.7.1',
-        'portpicker==1.3.0',
-        'colorama==0.4.1',
-        'packaging==19.0',
-        'SQLAlchemy==1.2.18'
-    ])
+    install_requires=requirements
+    )
