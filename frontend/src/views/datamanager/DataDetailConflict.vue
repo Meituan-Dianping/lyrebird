8fb24551e46b7d16e12dfea9b2c6e761ef85fa7d
yumiguan <yumiguan1009@gmail.com>
2019-07-16 16:20:32 +0800

    update conflict display and text

--- frontend/src/views/datamanager/DataDetailConflict.vue
+++ frontend/src/views/datamanager/DataDetailConflict.vue
@@ -1,28 +1,17 @@
 <template>
-  <div v-if="information && information.length">
-    <Row v-for="(value, index) in information" :key="index" style="padding-top:10px;word-break:break-all">
-      <Col span="2" offset="1">
-        <p>NO.{{index + 1}}</p>
-        <p>{{value.data.name}}</p>
-      </Col>
-      <Col span="20" offset="1">
-        <Row>
-          <Col span="7">
-            <b>Name</b>
-          </Col>
-          <Col span="16" offset="1">
-            <b>Path</b>
-          </Col>
-        </Row>
-        <Row v-for="item in value.conflict_data" :key="item.id" style="padding-bottom:10px">
-          <Col span="7">
-            <span>{{item.name}}</span>
-          </Col>
-          <Col span="16" offset="1">
-            <span>{{item.abs_parent_path}}</span>
-          </Col>
-        </Row>
-      </Col>
+  <div v-if="information && information.length" style="margin:0px 5px 10px 10px">
+    <Row v-for="(value, index) in information" :key="index" style="margin-top:10px;word-break:break-all;border:1px solid #ddd;">
+      <Row style="background-color:#f8f8f9;padding:5px 0px 5px 0px">
+        <Col span="18">
+          <b style="padding:0px 5px 0px 10px">{{index + 1}}. {{value.data.name}}</b>
+        </Col>
+        <Col span="5" offset="1" align="right">
+          <p style="padding:0px 10px 0px 5px"><b>{{value.conflict_data.length}}</b> conflicts</p>
+        </Col>
+      </Row>
+      <Row v-for="item in value.conflict_data" :key="item.id" style="padding:5px 0px 5px 0px">
+        <p style="padding:0px 10px 0px 30px"><b>Path</b>: {{item.abs_parent_path}}</p>
+      </Row>
     </Row>
   </div>
   <div v-else class="data-detail-empty">
