fb40fc2633a55e66b029438afc53838d421636e9
yumiguan <yumiguan1009@gmail.com>
2019-08-01 11:58:46 +0800

    upgrade delete button

--- frontend/src/views/datamanager/DataDetailInfo.vue
+++ frontend/src/views/datamanager/DataDetailInfo.vue
@@ -1,9 +1,9 @@
 <template>
-  <Row type="flex" justify="center" align="middle" style="margin-bottom:10px;word-break:break-all;">
-    <Col span="1" align="right" style="padding:0px 10px">
-      <Icon type="md-remove-circle" class="delete-button" v-show="readOnly.indexOf(this.infoKey) === -1" @click.native="deleteInfoKey"/>
-    </Col>
-    <Col span="5" align="right" style="padding:0px 10px">
+  <Row type="flex" justify="center" align="middle" @mouseover.native="isMouseOver=true" @mouseout.native="isMouseOver=false" style="margin-bottom:10px;word-break:break-all;">
+    <Col span="6" align="right" style="padding:0px 10px">
+      <Tooltip :content="this.readOnly.indexOf(this.infoKey)===-1 ? 'Delete': 'Undeletable key'" :delay="500">
+        <Icon type="md-remove-circle" :class="buttonClass" v-show="isMouseOver" @click.native="deleteInfoKey" style="padding:0px 10px"/>
+      </Tooltip>
       <span>{{this.infoKey}}</span>
     </Col>
     <Col span="18" style="padding:0px 0px 0px 10px">
@@ -18,7 +18,8 @@ export default {
   props: ['infoValue', 'infoKey'],
   data() {
     return {
-      readOnly: ['id', 'rule', 'secondary_search_id']
+      readOnly: ['id', 'rule', 'secondary_search_id'],
+      isMouseOver: false
     }
   },
   computed: {
@@ -29,6 +30,13 @@ export default {
       set (val) {
         this.$store.commit('setGroupDetailItem', { key: this.infoKey, value: val })
         this.$store.commit('setIsGroupDetailChanged', true)
+      }
+    },
+    buttonClass() {
+      if (this.readOnly.indexOf(this.infoKey) === -1) {
+        return ['enable-button']
+      } else {
+        return ['disable-button']
       }
     }
   },
@@ -42,7 +50,10 @@ export default {
 </script>
 
 <style>
-.delete-button {
+.enable-button {
   cursor: pointer;
+}
+.disable-button {
+  color: #c5c8ce;
 }
 </style>
