509bc380f8620725a7b7b62d2a17fc0415a52a57
yumiguan <yumiguan1009@gmail.com>
2019-12-13 16:43:39 +0800

    support display qrcode when folder info is link

--- frontend/src/views/datamanager/DataDetailInfo.vue
+++ frontend/src/views/datamanager/DataDetailInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <Row type="flex" justify="center" align="middle" @mouseover.native="isMouseOver=true" @mouseout.native="isMouseOver=false" style="margin-bottom:10px;word-break:break-all;">
+  <Row type="flex" align="middle" @mouseover.native="isMouseOver=true" @mouseout.native="isMouseOver=false" style="margin-bottom:10px;word-break:break-all;">
     <Col span="5" align="right" style="padding:0px 10px 0px 0px">
       <Tooltip :content="deletable ? 'Delete': 'Undeletable key'" :delay="500" placement="bottom-start">
         <Icon 
@@ -13,22 +13,48 @@
       <span>{{infoKey}}</span>
     </Col>
     <Col span="19" style="padding:0px 0px 0px 10px">
-      <Input v-if="editable" v-model="inputvalue" type="textarea" :autosize="{ minRows: 1 }" size="small"/>
-      <span v-else>{{infoValue}}</span>
+      <span v-if="inputValueType === 'link'">
+        <Input v-model="inputValue" type="textarea" :autosize="{ minRows: 1 }" size="small" style="width:calc(100% - 30px);"/>
+        <Poptip placement="bottom" width="382" word-wrap @on-popper-show="qrcodeRemake">
+          <svg-icon class="ivu-icon" name="qrcode" scale="2.8" style="margin-left:3px;cursor: pointer;"/>
+          <div slot="content">
+            <div id="qrcodeobj" ref="qrcodeobj"/>
+          </div>
+        </Poptip>
+      </span>
+      <span v-else-if="inputValueType === 'input'">
+        <Input v-model="inputValue" type="textarea" :autosize="{ minRows: 1 }" size="small"/>
+      </span>
+      <span v-else>
+        {{infoValue}}
+      </span>
     </Col>
   </Row>
 </template>
 
 <script>
+import QRCode from 'qrcodejs2'
+import svgIcon from 'vue-svg-icon/Icon.vue'
+
 export default {
   props: ['infoValue', 'infoKey', 'editable', 'deletable'],
+  components: {
+    svgIcon
+  },
   data() {
     return {
+      qrcodeObj: '',
+      maxLengthDisplayQrcode: 1270,
       isMouseOver: false
     }
   },
+  mounted () {
+    if (this.inputValueType === 'link') {
+      this.qrcodeMethod()
+    }
+  },
   computed: {
-    inputvalue: {
+    inputValue: {
       get () {
         return this.$store.state.dataManager.groupDetail[this.infoKey]
       },
@@ -36,11 +62,20 @@ export default {
         this.$store.commit('setGroupDetailItem', { key: this.infoKey, value: val })
       }
     },
-    buttonClass() {
+    buttonClass () {
       if (this.deletable) {
         return ['enable-button']
       } else {
         return ['disable-button']
+      }
+    },
+    inputValueType () {
+      if (String(this.infoValue).match('(?=.*://)')) {
+        return 'link'
+      } else if (this.editable) {
+        return 'input'
+      } else {
+        return 'text'
       }
     }
   },
@@ -48,6 +83,21 @@ export default {
     deleteInfoKey() {
       if (this.deletable) {
         this.$store.commit('deleteGroupDetailItem', this.infoKey)
+      }
+    },
+    qrcodeMethod () {
+      this.qrcodeObj = new QRCode(this.$refs.qrcodeobj, {
+        width: 350,
+        height: 350,
+        text: this.inputValue,
+        correctLevel : QRCode.CorrectLevel.H
+      })
+    },
+    qrcodeRemake () {
+      if (this.infoValue.length > this.maxLengthDisplayQrcode) {
+        this.$bus.$emit('msg.error', 'Make qrcode failed: ' + this.maxLengthDisplayQrcode + ' character is allowed, current length is ' + this.infoValue.length)
+      } else {
+        this.qrcodeObj.makeCode(this.inputValue)
       }
     }
   }
--- lyrebird/version.py
+++ lyrebird/version.py
@@ -1,3 +1,3 @@
-IVERSION = (1, 8, 1)
+IVERSION = (1, 8, 2)
 VERSION = ".".join(str(i) for i in IVERSION)
 LYREBIRD = "Lyrebird " + VERSION
