9dc01f7ade3e54880c31d475ac2a8b26f6a75ec2
yumiguan <yumiguan1009@gmail.com>
2020-04-02 16:38:49 +0800

    add space line in content_type/json.py

--- lyrebird/mock/handlers/http_data_helper/content_type/json.py
+++ lyrebird/mock/handlers/http_data_helper/content_type/json.py
@@ -7,6 +7,7 @@ class JsonHandler:
     def origin2flow(request_data):
         _data = json.loads(request_data.decode('utf-8'))
         return _data
+
     @staticmethod
     def flow2origin(flow_data):
         _data = json.dumps(flow_data).encode()
