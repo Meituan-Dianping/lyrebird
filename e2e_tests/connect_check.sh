44e33ef54919175a5bf128630ddc29a66da191de
youngfreeFJS <471011042@qq.com>
2020-04-03 12:26:10 +0800

    fail test

---
+++ e2e_tests/connect_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+is_connect=0
+for ((i=1;i<5;i++))
+do
+  echo "$i"
+  code=`curl -o /dev/null --retry 3 --retry-max-time 8 -s -w %{http_code} 127.0.0.1:9090/api/status`
+  echo "$code"
+  base=200
+  if [[ $code -eq $base ]];then
+    echo "is eq"
+    is_connect=1
+    break
+  fi
+  sleep 3
+done
+echo $is_connect
+if [[ $is_connect -eq 0 ]];then
+  ps -ef|grep serve.py | grep -v grep|awk '{printf $2}'|xargs kill -9
+  ps -ef|grep lyrebird | grep -v grep|awk '{printf $2}'|xargs kill -9
+  echo "lyrebird unconneted"
+  exit 8
+fi
+echo "last"
