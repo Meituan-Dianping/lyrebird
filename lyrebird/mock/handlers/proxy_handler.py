2d56527af5cb3b26976de76d4485dfe0def6e25e
yumiguan <yumiguan1009@gmail.com>
2020-07-22 17:51:40 +0800

    update code

--- lyrebird/mock/handlers/proxy_handler.py
+++ lyrebird/mock/handlers/proxy_handler.py
@@ -6,7 +6,6 @@ from .. import context
 from lyrebird import application
 from lyrebird.log import get_logger
 from .duplicate_request_handler import DuplicateRequest
-from .unproxy_request_handler import UnproxyRequest
 
 # 关闭ssl警告
 urllib3.disable_warnings()
@@ -29,12 +28,12 @@ class ProxyHandler:
         logger.info(f'<Proxy> {origin_url}')
 
         if not origin_url:
-            UnproxyRequest().handle(handler_context)
+            handler_context.is_proxiable = False
             return
 
         parsed_url = urllib.parse.urlparse(origin_url)
         if not parsed_url.hostname:
-            UnproxyRequest().handle(handler_context)
+            handler_context.is_proxiable = False
             return
         elif parsed_url.hostname in ['localhost', '127.0.0.1', ] and parsed_url.port == application.config["mock.port"]:
             DuplicateRequest().handle(handler_context)
