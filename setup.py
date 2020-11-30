2db97f857b2ed993ca9b3a9edbab0eb3788b96c5
zhaoye <zy81218@gmail.com>
2020-11-30 11:28:07 +0800

    update readme for v1.15.2

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: MacOS",
     ],
