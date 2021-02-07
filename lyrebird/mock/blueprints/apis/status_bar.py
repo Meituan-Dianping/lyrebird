708ebdaf5988c77a634514829bd7cd101b6dce9a
yumiguan <yumiguan1009@gmail.com>
2021-02-07 20:29:08 +0800

    change name

--- lyrebird/mock/blueprints/apis/status_bar.py
+++ lyrebird/mock/blueprints/apis/status_bar.py
@@ -8,7 +8,7 @@ class StatusBar(Resource):
         if item_id == None:
             return self._get_all_status_item()
         else:
-            return self._get_menu_by_status_item_id(item_id)
+            return self._get_menu_by_status_item_name(item_id)
 
     def _get_all_status_item(self):
         all_status_item = []
@@ -19,12 +19,12 @@ class StatusBar(Resource):
                 all_status_item.append({'id': status_item.name, 'text': status_item.get_text()})
         return application.make_ok_response(data=all_status_item)
 
-    def _get_menu_by_status_item_id(self, item_id):
+    def _get_menu_by_status_item_name(self, name):
         plugin_server = application.server['plugin']
         for plugin_name in plugin_server.plugins:
             plugin = plugin_server.plugins[plugin_name]
             for status_item in plugin.status:
-                if status_item.name == item_id:
+                if status_item.name == name:
                     menu_list = [menu_item.json() for menu_item in status_item.get_menu()]
                     return application.make_ok_response(data=menu_list)
-        return application.make_fail_response(f'Status item not found, id={item_id}')
+        return application.make_fail_response(f'Status item not found, id={name}')
