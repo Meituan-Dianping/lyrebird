164b894f94594a8704c8a853b764896c3d893bd4
ye zhao <zhaoye@users.noreply.github.com>
2021-05-11 15:47:28 +0800

    Feature/refactor e2e tests (#493)
    * update e2e test
    
    * update e2e test cases; fix: proxy server not stop
    
    * Add new line
    
    * Fixa e2e tests bug

--- e2e_tests/test_core.py
+++ e2e_tests/test_core.py
@@ -12,45 +12,38 @@ mock_uri = "http://127.0.0.1:9090/mock/"
 uri = mock_uri + serve_uri
 
 
-def test_flow_editor_img_data(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_img_data(lyrebird, mock_server):
     with open(f"{curPath}/assets/1.png", "rb") as f:
         data = f.read()
-    r = requests.post(url=uri, data=data, headers={'Content-Type': 'application/octet-stream'})
+    r = requests.post(url=uri, data=data)
     assert r.text == hashlib.md5(serve_uri.encode() + data).hexdigest()
 
-def test_flow_editor_img_file(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_img_file(lyrebird, mock_server):
     files = {'file': ('1.png', open(f'{curPath}/assets/1.png', 'rb'), 'image/jpg')}
     r = requests.post(uri, files=files)
     with open(f'{curPath}/assets/1.png', 'rb') as f:
         data = f.read()
     assert r.text == hashlib.md5(serve_uri.encode() + data).hexdigest()
 
-def test_flow_editor_json(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_json(lyrebird, mock_server):
     data = json.dumps({"name": {"12": 123}}, ensure_ascii=False)
     headers = {"Content-Type": "application/json"}
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
-def test_flow_editor_text(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_text(lyrebird, mock_server):
     data = "asdasdasd"
     headers = {"Content-Type": "text/plain"}
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
-def test_flow_editor_form(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_form(lyrebird, mock_server):
     data = 'z=9&a=1&a=2&b=1'
-    after_data = 'z=9&a=1&b=1'
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     r = requests.post(url=uri, data=data, headers=headers)
-    assert r.text == hashlib.md5(serve_uri.encode() + after_data.encode()).hexdigest()
+    assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
-def test_flow_editor_json_gzip(lyrebird_with_args, mock_server):
-    lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+def test_json_gzip(lyrebird, mock_server):
     data = {"a": 1}
     ziped_data = gzip.compress(json.dumps(data, ensure_ascii=False).encode())
     headers = {"Content-Type": "application/json", "Content-Encoding": "gzip"}
