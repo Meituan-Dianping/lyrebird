79f1d194e1837d8df52471aab12f48e5a0b12ffa
yumiguan <yumiguan1009@gmail.com>
2021-07-05 17:42:53 +0800

    Fix error of e2e_test

--- e2e_tests/test_core.py
+++ e2e_tests/test_core.py
@@ -1,25 +1,35 @@
 import pytest
+from jinja2 import Template
 import os, hashlib, json, gzip, requests
 
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 
 
-serve_uri = "http://127.0.0.1:5000/e2e_serve"
-serve_status_uri = "http://127.0.0.1:5000/status"
-api_uri = "http://127.0.0.1:9090/api/"
-mock_uri = "http://127.0.0.1:9090/mock/"
-uri = mock_uri + serve_uri
+serve_uri_template = "http://127.0.0.1:{{mock_server_port}}/e2e_serve"
+mock_uri_template = "http://127.0.0.1:{{lyrebird_port}}/mock/"
+uri_template = mock_uri_template + serve_uri_template
 
+
+def _formate_uri(uri, mock_server_port=None, lyrebird_port=None):
+    uri_template = Template(uri)
+    return uri_template.render({
+        'mock_server_port': mock_server_port,
+        'lyrebird_port': lyrebird_port
+    })
 
 def test_img_data(lyrebird, mock_server):
     with open(f"{curPath}/assets/1.png", "rb") as f:
         data = f.read()
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data)
     assert r.text == hashlib.md5(serve_uri.encode() + data).hexdigest()
 
 def test_img_file(lyrebird, mock_server):
     files = {'file': ('1.png', open(f'{curPath}/assets/1.png', 'rb'), 'image/jpg')}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(uri, files=files)
     with open(f'{curPath}/assets/1.png', 'rb') as f:
         data = f.read()
@@ -28,18 +38,24 @@ def test_img_file(lyrebird, mock_server):
 def test_json(lyrebird, mock_server):
     data = json.dumps({"name": {"12": 123}}, ensure_ascii=False)
     headers = {"Content-Type": "application/json"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
 def test_text(lyrebird, mock_server):
     data = "asdasdasd"
     headers = {"Content-Type": "text/plain"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
 def test_form(lyrebird, mock_server):
     data = 'z=9&a=1&a=2&b=1'
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
@@ -47,5 +63,7 @@ def test_json_gzip(lyrebird, mock_server):
     data = {"a": 1}
     ziped_data = gzip.compress(json.dumps(data, ensure_ascii=False).encode())
     headers = {"Content-Type": "application/json", "Content-Encoding": "gzip"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=ziped_data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + ziped_data).hexdigest()
--- e2e_tests/test_flow_editor.py
+++ e2e_tests/test_flow_editor.py
@@ -1,27 +1,45 @@
+import time
 import pytest
+from jinja2 import Template
 import os, hashlib, json, gzip, requests
 
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 
 
-serve_uri = "http://127.0.0.1:5000/e2e_serve"
-serve_status_uri = "http://127.0.0.1:5000/status"
-api_uri = "http://127.0.0.1:9090/api/"
-mock_uri = "http://127.0.0.1:9090/mock/"
-uri = mock_uri + serve_uri
+serve_uri_template = "http://127.0.0.1:{{mock_server_port}}/e2e_serve"
+mock_uri_template = "http://127.0.0.1:{{lyrebird_port}}/mock/"
+uri_template = mock_uri_template + serve_uri_template
+
+
+def _formate_uri(uri, mock_server_port=None, lyrebird_port=None):
+    uri_template = Template(uri)
+    return uri_template.render({
+        'mock_server_port': mock_server_port,
+        'lyrebird_port': lyrebird_port
+    })
 
 
 def test_flow_editor_img_data(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     with open(f"{curPath}/assets/1.png", "rb") as f:
         data = f.read()
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird_with_args.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers={'Content-Type': 'application/octet-stream'})
     assert r.text == hashlib.md5(serve_uri.encode() + data).hexdigest()
 
 def test_flow_editor_img_file(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     files = {'file': ('1.png', open(f'{curPath}/assets/1.png', 'rb'), 'image/jpg')}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird_with_args.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(uri, files=files)
     with open(f'{curPath}/assets/1.png', 'rb') as f:
         data = f.read()
@@ -29,30 +47,53 @@ def test_flow_editor_img_file(lyrebird_with_args, mock_server):
 
 def test_flow_editor_json(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     data = json.dumps({"name": {"12": 123}}, ensure_ascii=False)
     headers = {"Content-Type": "application/json"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird_with_args.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
+def test_empty_case(lyrebird_with_args, mock_server):
+    pass
+
 def test_flow_editor_text(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     data = "asdasdasd"
     headers = {"Content-Type": "text/plain"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird_with_args.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + data.encode()).hexdigest()
 
 def test_flow_editor_form(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     data = 'z=9&a=1&a=2&b=1'
     after_data = 'z=9&a=1&b=1'
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    uri = _formate_uri(uri_template, mock_server_port=mock_server.port, lyrebird_port=lyrebird_with_args.port)
+    serve_uri = _formate_uri(serve_uri_template, mock_server_port=mock_server.port)
     r = requests.post(url=uri, data=data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + after_data.encode()).hexdigest()
 
 def test_flow_editor_json_gzip(lyrebird_with_args, mock_server):
     lyrebird_with_args.start(checker_path='./assets/flow_editor.py')
+    # Wait for checker to load
+    time.sleep(3)
+
     data = {"a": 1}
     ziped_data = gzip.compress(json.dumps(data, ensure_ascii=False).encode())
     headers = {"Content-Type": "application/json", "Content-Encoding": "gzip"}
+    uri = f'http://127.0.0.1:{lyrebird_with_args.port}/mock/http://127.0.0.1:{mock_server.port}/e2e_serve'
+    serve_uri = f'http://127.0.0.1:{mock_server.port}/e2e_serve'
     r = requests.post(url=uri, data=ziped_data, headers=headers)
     assert r.text == hashlib.md5(serve_uri.encode() + ziped_data).hexdigest()
