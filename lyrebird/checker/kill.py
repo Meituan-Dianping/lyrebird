fc0e0584adaae8ba6e35cfe4e3914f4ec5b4f31e
yumiguan <yumiguan1009@gmail.com>
2021-11-11 16:37:43 +0800

    Update checker kill

--- lyrebird/checker/kill.py
+++ lyrebird/checker/kill.py
@@ -4,8 +4,9 @@ def kill_flow(flow):
     request_headers = flow['request']['headers']
     if not request_headers.get(headers.MITMPROXY_CLIENT_ADDRESS):
         return
-    flow['request']['headers'][headers.MITMPROXY_COMMAND] = 'kill'
 
     flow['response']['code'] = 0
     flow['response']['headers'] = {}
     flow['response']['data'] = ''
+
+    flow['response']['headers'][headers.MITMPROXY_COMMAND] = 'kill'
