de1c0bc8f88aae402dd85c3f6faab0627c5db413
yumiguan <yumiguan1009@gmail.com>
2021-12-24 17:49:01 +0800

    New vuetify theme color

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -17,25 +17,37 @@ const opts = {
     themes: {
       light: {
         primary: '#5F5CCA',
-        secondary: '#515a6e',
-        accent: '#000520',
+        primaryLight: '#6A67D4',
+        primaryBright: '#7B79D0',
+
         error: '#FF4B4B',
         warning: '#2d8cf0',
         info: '#ffc107',
         success: '#ff5722',
+
+        accent: '#000520',
+        content: '#9B9CB7',
+        
+        border: '#F1F0F4',
         shading: '#FAF9FA',
-        background: '#FFF'
+        background: '#FFF',
       },
       dark: {
         primary: '#5F5CCA',
-        secondary: '#424242',
-        accent: '#fff',
+        primaryLight: '#6A67D4',
+        primaryBright: '#7B79D0',
+
         error: '#FF5252',
         warning: '#FB8C00',
         info: '#2196F3',
         success: '#4CAF50',
+
+        accent: '#fff',
+        content: '#9B9CB7',
+
+        border: '#F1F0F4',
         shading: '#222222',
-        background: '#121212'
+        background: '#121212',
       },
     },
   },
