fa00e7ce05e50c1043ca131b4227df37e356d55a
liyilun <liyilun02@meituan.com>
2022-08-11 15:02:31 +0800

    check before sys.modules.pop

--- lyrebird/plugins/plugin_loader.py
+++ lyrebird/plugins/plugin_loader.py
@@ -136,8 +136,10 @@ def load_from_path(plugin_path):
         manifest_file = Path(plugin_path)/pkg/'manifest.py'
         if not manifest_file.exists():
             continue
-        sys.modules.pop(pkg)
-        sys.modules.pop(pkg+'.manifest')
+        if pkg in sys.modules:
+            sys.modules.pop(pkg)
+        if pkg+'.manifest' in sys.modules:
+            sys.modules.pop(pkg+'.manifest')
         imp.load_package(pkg, Path(plugin_path)/pkg)
         __import__(pkg+'.manifest')
 
