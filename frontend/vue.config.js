08c505c1ee932352220e83011c98a01fefd72a8a
zhaoye <zy81218@gmail.com>
2022-08-30 14:13:59 +0800

    Fix datamanager memory leak

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,9 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
-    plugins: [
-      new MonacoWebpackPlugin()
-    ]
+    plugins: [new MonacoWebpackPlugin()],
   },
   publicPath: '/ui/static',
   productionSourceMap: false,
@@ -13,17 +11,17 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-          target: 'http://localhost:9090'
+        target: 'http://localhost:9090',
       },
       '/*plugin*': {
-          target: 'http://localhost:9090'
+        target: 'http://localhost:9090',
       },
       '/static': {
-         target: 'http://localhost:9090'
+        target: 'http://localhost:9090',
       },
       '/socket.io': {
-        target: 'http://localhost:9090'
-      }
-  }
-  }
+        target: 'http://localhost:9090',
+      },
+    },
+  },
 }
