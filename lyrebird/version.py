1b8b561121b2c329259cd4a0aa6940371894f6f0
codeFly <chfeila@163.com>
2022-12-09 14:40:19 +0800

    解决版本冲突

--- lyrebird/version.py
+++ lyrebird/version.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 IVERSION = (2, 11, 0)
-=======
-IVERSION = (2, 10, 3)
->>>>>>> d66fe515d301fb423356ac8d7e03295c4330f3b2
 VERSION = ".".join(str(i) for i in IVERSION)
 LYREBIRD = "Lyrebird " + VERSION
