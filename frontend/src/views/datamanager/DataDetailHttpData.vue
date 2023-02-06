776ede0e4e5c8829f7c86429f3d57092476c2cd0
codeFly <chfeila@163.com>
2023-02-06 11:56:12 +0800

    flow详情页展示增加responseData的diff入口

--- frontend/src/views/datamanager/DataDetailHttpData.vue
+++ frontend/src/views/datamanager/DataDetailHttpData.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="small-tab">
-    <tabs v-model="currentTab" :animated="false" size="small">
+    <tabs @on-click="onRespDataTabClick" v-model="currentTab" :animated="false" size="small">
       <tab-pane label="Information" name="info" />
       <tab-pane label="Request" name="req" />
       <tab-pane label="RequestData" name="reqData" />
@@ -9,14 +9,23 @@
     </tabs>
     <div>
       <CodeEditor
+        v-if="!isShowCodeDiffEditor"
         class="data-detail"
         :language="currentTabContentType"
         v-model="editorContent"
         v-on:on-jsonpath-change="onJsonPathChange"
       ></CodeEditor>
+      <CodeDiffEditor
+        v-if="isShowCodeDiffEditor"
+        class="data-detail"
+        :language="currentTabContentType"
+        :content="editorContent"
+        :diffContent="editorDiffContent"
+        :readOnly="true"
+      ></CodeDiffEditor>
     </div>
     <div class="save-btn" v-if="dataDetail">
-      <v-tooltip top>
+      <!-- <v-tooltip top>
         <template v-slot:activator="{ on, attrs }">
           <v-btn
             v-bind="attrs"
@@ -27,7 +36,7 @@
             class="save-btn-detail"
             @click="save"
           >
-            <v-icon 
+            <v-icon
             class="save-btn-icon"
             dark>
               mdi-content-save-outline
@@ -35,7 +44,56 @@
           </v-btn>
         </template>
         <span>Save (⌘+s)</span>
-      </v-tooltip>
+      </v-tooltip> -->
+
+      <!-- <v-card id="create"> -->
+      <v-speed-dial
+        :open-on-hover="true"
+      >
+        <template v-slot:activator>
+          <v-btn
+            color="primary"
+            dark
+            fab
+            class="save-btn-detail"
+            @click="save"
+            title="Save (⌘+s)"
+          >
+            <v-icon
+            class="save-btn-icon"
+            dark>
+              mdi-content-save-outline
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-btn
+          fab
+          dark
+          small
+          color="primary"
+          class="save-btn-detail"
+          :value="currentTab"
+          @click="showDiff"
+          title="Show the differences"
+        >
+          <v-icon>mdi-eye-outline</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="primary"
+          class="save-btn-detail"
+          :value="currentTab"
+          @click="hideDiff"
+          title="Hide the differences"
+        >
+          <v-icon>mdi-eye-off-outline</v-icon>
+        </v-btn>
+      </v-speed-dial>
+    <!-- </v-card> -->
+
+
     </div>
   </div>
 </template>
@@ -44,11 +102,13 @@
 import DataDetailInfo from '@/views/datamanager/DataDetailInfo.vue'
 import CodeEditor from '@/components/CodeEditor.vue'
 import Icon from 'vue-svg-icon/Icon.vue'
+import CodeDiffEditor from '@/components/CodeDiffEditor.vue'
 
 export default {
   components: {
     DataDetailInfo,
     CodeEditor,
+    CodeDiffEditor,
     'svg-icon': Icon
   },
   data () {
@@ -58,8 +118,9 @@ export default {
         req: null,
         reqData: null,
         resp: null,
-        respData: null
-      }
+        respData: null,
+      },
+      isShowDiff: false,
     }
   },
   mounted () {
@@ -103,12 +164,48 @@ export default {
       set (val) {
         this.editorCache[this.currentTab] = val
       }
+    },
+    editorDiffContent: {
+      get () {
+        const content = this.$store.state.dataManager.renderedRespData
+        if (!content) {
+          return ''
+        }
+        return content
+      },
+      set (val) {
+        this.$store.commit('setRenderedRespData', val)
+      }
+    },
+    isShowCodeDiffEditor() {
+      if (this.currentTab === 'respData') {
+        if (this.isShowDiff){
+          return true
+        }
+      } else {
+        return false
+      }
+    },
+    isCurrentTabRespData () {
+      if (this.currentTab === 'respData') {
+        return true
+      } else {
+        return false
+      }
+    },
+    isShowEye () {
+      if (this.editorContent != this.editorDiffContent && !this.isShowDiff) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   watch: {
     dataDetail (val) {
       this.setDataDetailEditorCache(val)
-    }
+      this.onRespDataTabClick()
+    },
   },
   methods: {
     save () {
@@ -160,7 +257,18 @@ export default {
         headers: val.response.headers
       })
       this.editorCache.respData = typeof (val.response.data) == 'object' ? JSON.stringify(val.response.data) : val.response.data
-    }
+    },
+    onRespDataTabClick () {
+        const responseData = this.editorCache.respData
+        this.$store.dispatch('loadRenderedData', {'data': responseData})
+        console.log("12333")
+    },
+    showDiff () {
+      this.isShowDiff = true
+    },
+    hideDiff () {
+      this.isShowDiff = false
+    },
   }
 }
 </script>
