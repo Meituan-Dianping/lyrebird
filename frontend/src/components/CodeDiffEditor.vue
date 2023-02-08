c704c36c1ed36822c36bd1b142c5ecd4c818e6f1
codeFly <chfeila@163.com>
2023-02-08 18:47:48 +0800

    Modify front-end display error

--- frontend/src/components/CodeDiffEditor.vue
+++ frontend/src/components/CodeDiffEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="code-diff-editor" class="pa-3" style="width:100%;height:100%;border-top:1px;solid grey"></div>
+    <div id="code-diff-editor" class="pa-3" style="width:100%;height:100%;border-top:1px solid grey"></div>
   </div>
 </template>
 
