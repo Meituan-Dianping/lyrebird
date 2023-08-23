121b2be6cb6be2fe224280e4b7a8817692618bf2
yumiguan <41277086+yumiguan@users.noreply.github.com>
2023-08-23 17:11:37 +0800

    Mock ssr add jsonpath to get part of mock data; Reporter error when server not init; Extra mock server add client address; jsonpath support return one result (#778)

--- lyrebird/config/keywords.py
+++ lyrebird/config/keywords.py
@@ -3,4 +3,5 @@ CONFIG_TREE_LOAD_CHILDREN_ASYNC= 'mock.data.tree.asynchronous'
 CONFIG_MOCK_REQUEST_SSR_MOCK_IN_BODY = 'mock.request.ssr.mock_in_body'
 CONFIG_MOCK_REQUEST_SSR_MOCK_BODY_KEY = 'mock.request.ssr.mock_body_key'
 CONFIG_MOCK_REQUEST_SSR_MOCK_IN_BODY_PARAM = 'mock.request.ssr.mock_in_body.param'
+CONFIG_MOCK_REQUEST_SSR_MOCK_DATA_JSONPATH = 'mock.request.ssr.mock_data_jsonpath'
 
