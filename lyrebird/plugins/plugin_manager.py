aba42097ca70010748454ce8f6b769a45d3573f9
noO0oOo0ob <625171796@qq.com>
2024-02-05 12:09:09 +0800

    add plugin upstream param

--- lyrebird/plugins/plugin_manager.py
+++ lyrebird/plugins/plugin_manager.py
@@ -111,7 +111,8 @@ class PluginManager(StaticServer):
                     'name': handler[0],
                     'func': handler[1],
                     'rules': handler[2] if len(handler) > 2 else None,
-                    'rank': handler[3] if len(handler) > 3 and isinstance(handler[3], (int, float)) else 0
+                    'rank': handler[3] if len(handler) > 3 and isinstance(handler[3], (int, float)) else 0,
+                    'modifiy_request_body': handler[4] if len(handler) > 4 else True
                 })
 
             # Subscribe handler on proxy response
