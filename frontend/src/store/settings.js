c8c47f2edb16eb0c193ca37a2cba170c6dc76796
noO0ob <wujiasheng03@meituan.com>
2024-10-24 10:23:28 +0800

    settings page frontend create

--- frontend/src/store/settings.js
+++ frontend/src/store/settings.js
@@ -28,7 +28,10 @@ export default {
   state: {
     config: {},
     initialized: false,
-    preLoadFuncSet: new Set()
+    preLoadFuncSet: new Set(),
+    focusSettingPanel: '',
+    settingsList: [],
+    settingsCurrentDetail: {},
   },
   mutations: {
     setConfig (state, config) {
@@ -42,7 +45,16 @@ export default {
     },
     deletePreLoadFuncSet (state, preLoadFunc) {
       state.preLoadFuncSet.delete(preLoadFunc)
-    }
+    },
+    setSettingsList(state, data) {
+      state.settingsList = data
+    },
+    setSettingsCurrentDetail(state, data) {
+      state.settingsCurrentDetail = data
+    },
+    setFocusSettingPanel(state, data) {
+      state.focusSettingPanel = data
+    },
   },
   actions: {
     loadConfig({ state, commit, dispatch }) {
@@ -98,6 +110,124 @@ export default {
         .catch(error => {
           bus.$emit('msg.error', `Update config failed ${error.data.message}`)
         })
-    }
-  }
+    },
+    loadSettingsList({ state, commit, dispatch }) {
+      // api.getSettingModelList()
+      //   .then(response => {
+      //     commit('setSettingsList', response.data)
+      //   })
+      //   .catch(error => {
+      //     bus.$emit('msg.error', 'Load config failed ' + error.data.message)
+      //   })
+      let data = [
+        {
+          'category': '账号相关',
+          'scripts': [
+            {
+              'name': 'account_info',
+              'title': '账号信息',
+              'notice': '账号信息展示内容',
+              'category': '账号相关'
+            },
+            {
+              'name': 'signout',
+              'title': '账号登出',
+              'notice': '登出账号',
+              'category': '账号相关'
+            }
+          ]
+        },
+        {
+          'category': '代理相关',
+          'scripts': [
+            {
+              'name': 'proxy_white_list',
+              'title': '代理白名单配置',
+              'notice': '设置Lyrebird代理过程中过滤不进入Lyrebird代理的域名',
+              'category': '代理相关'
+            }
+          ]
+        },
+        {
+          'category': 'Extension相关',
+          'scripts': [
+            {
+              'name': 'extension_lock_update',
+              'title': '冻结checker',
+              'notice': '锁定本地checker,使得其不受远程仓库更新影响',
+              'category': 'Extension相关'
+            }
+          ]
+        }
+      ]
+      commit('setSettingsList', data)
+    },
+    loadSettingsForm({ state, commit, dispatch }, payload) {
+      // api.getSettingsForm(payload)
+      //   .then(response => {
+      //     commit('setSettingsCurrentDetail', response.data)
+      //   })
+      //   .catch(error => {
+      //     bus.$emit('msg.error', 'Load config failed ' + error.data.message)
+      //   })
+      let data = {
+        'name': 'proxy_white_list',
+        'title': '代理白名单配置',
+        'subtitle': '设置Lyrebird代理过程中过滤不进入Lyrebird代理的域名',
+        'language': 'cn',
+        'submitText': '',
+        'configs': [
+          {
+            'name': 'name1',
+            'title': '标题1标题1标题1标题1',
+            'subtitle': '这是一段描述',
+            'category': 'text',
+            'data': 'http://i.meituan.com'
+          },
+          {
+            'name': 'name2',
+            'title': '标题2标题2标题2标题2',
+            'subtitle': '这是一段描述',
+            'category': 'selector',
+            'data': 'option1',
+            'options': ['option1', 'option2', 'option3']
+          },
+          {
+            'name': 'name3',
+            'title': '标题3标题3标题3',
+            'subtitle': '这是一段描述',
+            'category': 'dict',
+            'data': {
+              'Key1': 'value1',
+              'Key2': 'value2',
+              'Key3': 'value3'
+            }
+          },
+          {
+            'name': 'name4',
+            'title': '标题4标题4标题4',
+            'subtitle': '这是一段描述',
+            'category': 'list',
+            'data': [
+              'png',
+              'jpeg',
+              'json'
+            ]
+          },
+        ]
+      }
+      commit('setSettingsCurrentDetail', data)
+    },
+    saveSettingsForm({ state, commit, dispatch }, { formName, formData }) {
+      // api.setSettingsForm(payload)
+      //   .then(response => {
+      //     bus.$emit('msg.success', 'Data ' + payload.name + ' update!')
+      //   })
+      //   .catch(error => {
+      //     bus.$emit('msg.error', 'Data ' + payload.name + ' update error: ' + error.data.message)
+      //   })
+      // TODO 异步的时候，检测当前panel的name是否还是load的name，如果不是就不再重新加载
+      dispatch('loadSettingsForm', formName)
+    },
+  },
 }
